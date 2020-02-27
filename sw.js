5a0e73fa0abec0ed8ce797296d81f4138446ee53
dasoljong92 <56146490+dasoljong92@users.noreply.github.com>
2020-02-28 01:44:02 +0900

    Updates

--- sw.js
+++ sw.js
@@ -42,14 +42,14 @@ self.__precacheManifest = [
     "url": "netlify-identity-widget-8e6f024872c0d48ae324.js"
   },
   {
-    "url": "app-4881c9fe6541e4b3efd9.js"
+    "url": "app-18dac7280e0f14cc15a4.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-cf2fd0f92da7499f18b1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1d33d26f15bcfe5e976f30ea5a848267"
+    "revision": "cb466ee9fffc05da53a0f7a9e4934d7b"
   },
   {
     "url": "manifest.json",
@@ -150,7 +150,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-4881c9fe6541e4b3efd9.js`))) {
+  if (!resources || !(await caches.match(`/app-18dac7280e0f14cc15a4.js`))) {
     return await fetch(event.request)
   }
 
