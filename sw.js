e27587bc2788b99459d08bd621001d52cedeb9be
DasolJong <dasoljong92@gmail.com>
2020-07-23 00:35:24 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "styles-117479406f6e61ad4793.js"
   },
   {
-    "url": "commons-f55e85427dacf2112d21.js"
+    "url": "commons-dc988ff6a7d52cab1907.js"
   },
   {
     "url": "netlify-identity-widget-8e6f024872c0d48ae324.js"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bce33af213470fb34d554fb8cafa8f3b"
+    "revision": "dd72118b7ee7783b19dd3f2bfbbfbbab"
   },
   {
     "url": "manifest.json",
